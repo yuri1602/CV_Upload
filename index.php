b4e3740e6b4ffa75764a30fc4ba05aa872e0bc74
Yuri Metodiev <juri1602@gmail.com>
2023-10-24 21:56:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
     <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
