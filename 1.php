e1bfd4afbc74ba04cd44a6feb26c48e880846168
Yuri Metodiev <juri1602@gmail.com>
2023-10-24 21:39:51 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -97,7 +97,6 @@ if (isset($_POST['submit'])){
     <option value="Python">Python</option>
     <option value="C++">C++</option>
     <option value="Python">Python</option>
-    <option value="C++">C++</option>
     <option value="JavaScript">JavaScript</option>
     <option value="Java">Java</option>
     <option value="Ruby">Ruby</option>
